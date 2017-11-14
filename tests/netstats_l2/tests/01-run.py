a16491fe328c7eaf492d428e35e6fc54cb6af648
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-11-14 23:46:08 +0100

    tests/netstats_l2: migrate to testrunner

---
+++ tests/netstats_l2/tests/01-run.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2017 Alexandre Abadie <alexandre.abadie@inria.fr>
+#
+# This file is subject to the terms and conditions of the GNU Lesser
+# General Public License v2.1. See the file LICENSE in the top level
+# directory for more details.
+
+import os
+import sys
+
+EXPECTED_HELP = (
+    'Command              Description',
+    '---------------------------------------',
+    'reboot               Reboot the node',
+    'ifconfig             Configure network interfaces',
+    '>'
+)
+
+EXPECTED_IFCONFIG = (
+    r'ifconfig',
+    r'Iface  \d+   HWaddr\: ([a-f0-9]{2}[:-]){5}([a-f0-9]{2}) ',
+    r'       Source address length: \d+',
+    r'       Statistics for Layer 2',
+    r'        RX packets \d+  bytes \d+',
+    r'        TX packets \d+ \(Multicast: \d+\)  bytes \d+',
+    r'        TX succeeded \d+ errors 0'
+)
+
+
+def testfunc(child):
+    child.sendline('help')
+    for line in EXPECTED_HELP:
+        child.expect_exact(line)
+
+    child.sendline('ifconfig')
+    for line in EXPECTED_IFCONFIG:
+        child.expect(line)
+
+if __name__ == "__main__":
+    sys.path.append(os.path.join(os.environ['RIOTBASE'], 'dist/tools/testrunner'))
+    from testrunner import run
+    sys.exit(run(testfunc))
