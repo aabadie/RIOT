917f4517d650292593fb0544b34155e11aa2f6f9
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-02-20 16:49:49 +0100

    boards/adafruit-edgebadge: add initial support (broken)

---
+++ boards/adafruit-edgebadge/include/board.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2020 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_adafruit-edgebadge
+ * @{
+ *
+ * @file
+ * @brief       Board specific definitions for the Adafruit EdgeBadge
+ *
+ * @author      Alexandre Abadie <alexandre.abadie@inria.fr>
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "cpu.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @name    LED pin definitions and handlers
+ * @{
+ */
+#define LED0_PIN            GPIO_PIN(PA, 23)
+
+#define LED_PORT            PORT->Group[PA]
+#define LED0_MASK           (1 << 23)
+
+#define LED0_ON             (LED_PORT.OUTSET.reg = LED0_MASK)
+#define LED0_OFF            (LED_PORT.OUTCLR.reg = LED0_MASK)
+#define LED0_TOGGLE         (LED_PORT.OUTTGL.reg = LED0_MASK)
+
+#define LED0_NAME           "LED(Red)"
+/** @} */
+
+/**
+ * @name    Xtimer configuration
+ * @{
+ */
+#define XTIMER_WIDTH                (32)
+#define XTIMER_HZ                   (1000000ul)
+/** @} */
+
+/**
+ * @brief Initialize board specific hardware, including clock, LEDs and std-IO
+ */
+void board_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_H */
+/** @} */
