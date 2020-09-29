b3d9c87db9dd66b682957c41c05aa7247088bbd3
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-09-29 14:00:54 +0200

    boards/nucleo32: configure specific LED0 for nucleo-g431kb

--- boards/common/nucleo32/include/board.h
+++ boards/common/nucleo32/include/board.h
@@ -33,8 +33,13 @@ extern "C" {
  * @name Macros for controlling the on-board LED (LD3).
  * @{
  */
+#if defined(BOARD_NUCLEO_G431KB)
+#define LED0_PIN            GPIO_PIN(PORT_B, 8)
+#define LED0_MASK           (1 << 8)
+#else
 #define LED0_PIN            GPIO_PIN(PORT_B, 3)
 #define LED0_MASK           (1 << 3)
+#endif
 #define LED0_ON             (GPIOB->BSRR = LED0_MASK)
 #define LED0_OFF            (GPIOB->BSRR = (LED0_MASK << 16))
 #define LED0_TOGGLE         (GPIOB->ODR  ^= LED0_MASK)
