abe15fc8c5cd6b8f7eb133e80792f35d9ca43764
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-11-19 21:42:16 +0100

    makefiles/adafruit-nrfutil: fix flash-only target

--- makefiles/tools/adafruit-nrfutil.inc.mk
+++ makefiles/tools/adafruit-nrfutil.inc.mk
@@ -3,6 +3,6 @@ FLASHDEPS += $(HEXFILE).zip
 FLASHER = adafruit-nrfutil
 FFLAGS = dfu serial --port=${PORT} --baudrate=${BAUD} --touch=1200 --package=$(HEXFILE).zip --singlebank
 
-%.hex.zip: %.hex
+%.hex.zip: $(HEXFILE)
 	$(call check_cmd,$(FLASHER),Flash program and preparation tool)
 	$(FLASHER) dfu genpkg --dev-type 0x0052 --sd-req 0x00B6 --application $< $@
